3797c0bea9d8da2239bd5f8a15f7025251401471
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-01-22 20:27:06 -0700

    Added permission safeguard.

--- source/command-handler/action-ban.js
+++ source/command-handler/action-ban.js
@@ -23,59 +23,81 @@ module.exports = {
       admin: interaction.user.id,
     };
 
-    let success = 0;
-    const gameserver = async (reference, services) => {
-      const action = async (service) => {
-        try {
-          const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/banlist`;
-          const response = await axios.post(url, { identifier: input.username }, { headers: { 'Authorization': reference.nitrado.token } });
-          response.status === 200 ? success++ : unauthorized();
-          console.log(response.data.data.message);
-        } catch (error) { if (error.response.data.message === "Can't add the user to the banlist.") { success++ }; };
-      };
+    await interaction.guild.roles.fetch().then(async roles => {
+      const role = roles.find(role => role.name === 'Obelisk Permission');
 
-      const filter = async (service) => {
-        platforms[service.details.folder_short] ? await action(service) : console.log('Incompatible gameserver.')
-      };
-
-      const tasks = await services.map(async service => await filter(service));
-
-      await Promise.all(tasks).then(async () => {
-
+      if (!role || !interaction.member.roles.cache.has(role.id)) {
         const embed = new EmbedBuilder()
-          .setColor('#2ecc71')
-          .setDescription(`**Game Command Success**\nGameserver action completed.\nExecuted on \`${success}\` of \`${action.length}\` servers.\n<t:${Math.floor(Date.now() / 1000)}:f>`)
+          .setColor('#e67e22')
+          .setDescription(`**Unauthorized Access**\nYou do not have the required permissions.\nPlease ask an administrator for access.\n${role}\n\n ** Additional Information **\nThe role was generated upon token setup.`)
           .setFooter({ text: 'Tip: Contact support if there are issues.' })
-          .setThumbnail('https://i.imgur.com/CzGfRzv.png')
 
-        await interaction.followUp({ embeds: [embed] })
-          .then(async () => {
-            if (success) {
-              await db.collection('ase-player-banned').doc(input.guild).set({
-                [input.username]: { admin: input.admin, reason: input.reason }
-              }, { merge: true });
-            };
-          });
-      });
-    };
+        return interaction.followUp({ embeds: [embed], ephemeral: true });
+      };
 
-    const service = async (reference) => {
-      try {
-        const url = 'https://api.nitrado.net/services';
-        const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
-        response.status === 200 ? gameserver(reference, response.data.data.services) : unauthorized();
-      } catch (error) { unauthorized() };
-    };
+      const unauthorized = async () => {
+        const embed = new EmbedBuilder()
+          .setColor('#e67e22')
+          .setDescription(`**Unauthorized Access**\nYou do not have a connected account.\nPlease authorize with your provider.\n\`/setup-account\`\n\n**Additional Information**\nEnsure you follow setup procedures.`)
+          .setFooter({ text: 'Tip: Contact support if there are issues.' })
 
-    const token = async (reference) => {
-      try {
-        const url = 'https://oauth.nitrado.net/token';
-        const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
-        response.status === 200 ? service(reference) : unauthorized();
-      } catch (error) { unauthorized() };
-    };
+        return interaction.followUp({ embeds: [embed] });
+      };
 
-    const reference = (await db.collection('configuration').doc(input.guild).get()).data();
-    reference ? await token(reference) : unauthorized();
+      let success = 0;
+      const gameserver = async (reference, services) => {
+        const action = async (service) => {
+          try {
+            const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/banlist`;
+            const response = await axios.post(url, { identifier: input.username }, { headers: { 'Authorization': reference.nitrado.token } });
+            response.status === 200 ? success++ : unauthorized();
+            console.log(response.data.data.message);
+          } catch (error) { if (error.response.data.message === "Can't add the user to the banlist.") { success++ }; };
+        };
+
+        const filter = async (service) => {
+          platforms[service.details.folder_short] ? await action(service) : console.log('Incompatible gameserver.')
+        };
+
+        const tasks = await services.map(async service => await filter(service));
+
+        await Promise.all(tasks).then(async () => {
+
+          const embed = new EmbedBuilder()
+            .setColor('#2ecc71')
+            .setDescription(`**Game Command Success**\nGameserver action completed.\nExecuted on \`${success}\` of \`${action.length}\` servers.\n<t:${Math.floor(Date.now() / 1000)}:f>`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setThumbnail('https://i.imgur.com/CzGfRzv.png')
+
+          await interaction.followUp({ embeds: [embed] })
+            .then(async () => {
+              if (success) {
+                await db.collection('ase-player-banned').doc(input.guild).set({
+                  [input.username]: { admin: input.admin, reason: input.reason }
+                }, { merge: true });
+              };
+            });
+        });
+      };
+
+      const service = async (reference) => {
+        try {
+          const url = 'https://api.nitrado.net/services';
+          const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+          response.status === 200 ? gameserver(reference, response.data.data.services) : unauthorized();
+        } catch (error) { unauthorized() };
+      };
+
+      const token = async (reference) => {
+        try {
+          const url = 'https://oauth.nitrado.net/token';
+          const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+          response.status === 200 ? service(reference) : unauthorized();
+        } catch (error) { unauthorized() };
+      };
+
+      const reference = (await db.collection('ase-configuration').doc(input.guild).get()).data();
+      reference ? await token(reference) : unauthorized();
+    });
   }
 };
--- source/command-handler/action-unwhitelist.js
+++ source/command-handler/action-unwhitelist.js
@@ -21,60 +21,73 @@ module.exports = {
       admin: interaction.user.id,
     };
 
-    const unauthorized = async () => {
-      const embed = new EmbedBuilder()
-        .setColor('#e67e22')
-        .setDescription(`**Unauthorized Access**\nYou do not have a connected account.\nPlease authorize with your provider.\n\`/setup-account\`\n\n**Additional Information**\nEnsure you follow setup procedures.`)
-        .setFooter({ text: 'Tip: Contact support if there are issues.' })
+    await interaction.guild.roles.fetch().then(async roles => {
+      const role = roles.find(role => role.name === 'Obelisk Permission');
 
-      return interaction.followUp({ embeds: [embed] });
-    };
-
-    let success = 0;
-    const gameserver = async (reference, services) => {
-      const action = async (service) => {
-        try {
-          const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/whitelist`;
-          const response = await axios.delete(url, { headers: { 'Authorization': reference.nitrado.token }, data: { identifier: input.username } });
-          response.status === 200 ? success++ : unauthorized();
-          console.log(response.data.data.message);
-        } catch (error) { if (error.response.data.message === "Can't remove the user from the whitelist.") { success++ }; };
-      };
-
-      const filter = async (service) => {
-        platforms[service.details.folder_short] ? await action(service) : console.log('Incompatible gameserver.')
-      };
-
-      const tasks = await services.map(async service => await filter(service));
-
-      await Promise.all(tasks).then(async () => {
+      if (!role || !interaction.member.roles.cache.has(role.id)) {
         const embed = new EmbedBuilder()
-          .setColor('#2ecc71')
-          .setDescription(`**Game Command Success**\nGameserver action completed.\nExecuted on \`${success}\` of \`${action.length}\` servers.\n<t:${Math.floor(Date.now() / 1000)}:f>`)
+          .setColor('#e67e22')
+          .setDescription(`**Unauthorized Access**\nYou do not have the required permissions.\nPlease ask an administrator for access.\n${role}\n\n ** Additional Information **\nThe role was generated upon token setup.`)
           .setFooter({ text: 'Tip: Contact support if there are issues.' })
-          .setThumbnail('https://i.imgur.com/CzGfRzv.png')
 
-        await interaction.followUp({ embeds: [embed] })
-      });
-    };
+        return interaction.followUp({ embeds: [embed], ephemeral: true });
+      };
 
-    const service = async (reference) => {
-      try {
-        const url = 'https://api.nitrado.net/services';
-        const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
-        response.status === 200 ? gameserver(reference, response.data.data.services) : unauthorized();
-      } catch (error) { unauthorized() };
-    };
+      const unauthorized = async () => {
+        const embed = new EmbedBuilder()
+          .setColor('#e67e22')
+          .setDescription(`**Unauthorized Access**\nYou do not have a connected account.\nPlease authorize with your provider.\n\`/setup-account\`\n\n**Additional Information**\nEnsure you follow setup procedures.`)
+          .setFooter({ text: 'Tip: Contact support if there are issues.' })
 
-    const token = async (reference) => {
-      try {
-        const url = 'https://oauth.nitrado.net/token';
-        const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
-        response.status === 200 ? service(reference) : unauthorized();
-      } catch (error) { unauthorized() };
-    };
+        return interaction.followUp({ embeds: [embed] });
+      };
 
-    const reference = (await db.collection('configuration').doc(input.guild).get()).data();
-    reference ? await token(reference) : unauthorized();
+      let success = 0;
+      const gameserver = async (reference, services) => {
+        const action = async (service) => {
+          try {
+            const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/whitelist`;
+            const response = await axios.delete(url, { headers: { 'Authorization': reference.nitrado.token }, data: { identifier: input.username } });
+            response.status === 200 ? success++ : unauthorized();
+            console.log(response.data.data.message);
+          } catch (error) { if (error.response.data.message === "Can't remove the user from the whitelist.") { success++ }; };
+        };
+
+        const filter = async (service) => {
+          platforms[service.details.folder_short] ? await action(service) : console.log('Incompatible gameserver.')
+        };
+
+        const tasks = await services.map(async service => await filter(service));
+
+        await Promise.all(tasks).then(async () => {
+          const embed = new EmbedBuilder()
+            .setColor('#2ecc71')
+            .setDescription(`**Game Command Success**\nGameserver action completed.\nExecuted on \`${success}\` of \`${action.length}\` servers.\n<t:${Math.floor(Date.now() / 1000)}:f>`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setThumbnail('https://i.imgur.com/CzGfRzv.png')
+
+          await interaction.followUp({ embeds: [embed] })
+        });
+      };
+
+      const service = async (reference) => {
+        try {
+          const url = 'https://api.nitrado.net/services';
+          const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+          response.status === 200 ? gameserver(reference, response.data.data.services) : unauthorized();
+        } catch (error) { unauthorized() };
+      };
+
+      const token = async (reference) => {
+        try {
+          const url = 'https://oauth.nitrado.net/token';
+          const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+          response.status === 200 ? service(reference) : unauthorized();
+        } catch (error) { unauthorized() };
+      };
+
+      const reference = (await db.collection('ase-configuration').doc(input.guild).get()).data();
+      reference ? await token(reference) : unauthorized();
+    });
   }
 };

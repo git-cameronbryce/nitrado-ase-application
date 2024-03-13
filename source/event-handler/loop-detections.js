9e0daf515ca7f005d1ef0ac2c605138b9287d386
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-03-13 01:29:35 -0600

    Drafted protection.

---
+++ source/event-handler/loop-detections.js
@@ -0,0 +1,63 @@
+const { ActionRowBuilder, Events, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { db } = require('../script');
+const axios = require('axios');
+
+const platforms = { arkxb: true };
+const matches = new Set();
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+
+    async function loop() {
+
+      const gameserver = async (nitrado, protections, services) => {
+        const verify = async (player) => {
+          matches.add(player.name);
+          if (!matches.has(player.name)) {
+            console.log(player.name);
+          }
+        };
+
+        const filter = async (service, gameserver) => {
+          const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/players`;
+          const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
+          response.data.data.players.forEach(async player => {
+            if (player.online) { await verify(player) }
+          });
+        };
+
+        const tasks = await services.map(async service => {
+          if (platforms[service.details.folder_short]) {
+            const url = `https://api.nitrado.net/services/${service.id}/gameservers`;
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
+            if (response.status === 200 && response.data.data.gameserver.status === 'started') { await filter(service, response.data.data.gameserver) };
+          };
+        });
+      };
+
+      const service = async (nitrado, protections) => {
+        const url = 'https://api.nitrado.net/services';
+        const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+        const services = response.data.data.services;
+        response.status === 200 ? gameserver(nitrado, protections, services) : invalidService()
+      };
+
+      const token = async ({ nitrado, protections }) => {
+        try {
+          const url = 'https://oauth.nitrado.net/token';
+          const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+          response.status === 200 ? service(nitrado, protections) : console.log('Invalid token');
+        } catch (error) { null };
+      };
+
+      const reference = await db.collection('ase-configuration').get();
+      reference.forEach(doc => {
+        doc.data() ? token(doc.data()) : console.log('Invalid document.');
+      });
+      setTimeout(loop, 10000);
+    };
+    loop().then(() => console.log('Loop started:'));
+  },
+};

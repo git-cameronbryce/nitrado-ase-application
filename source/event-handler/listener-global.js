fca60854606eed70dbeaaa47cfda7cb6831b1e62
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-03-15 23:12:12 -0600

    Changed project naming.

---
+++ source/event-handler/listener-global.js
@@ -0,0 +1,264 @@
+const { ActionRowBuilder, Events, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { db } = require('../script.js');
+const axios = require('axios');
+
+const platforms = { arkxb: true, arkps: true, arkse: true };
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    client.on(Events.InteractionCreate, async interaction => {
+
+      if (interaction.customId === 'cluster-command') {
+        await interaction.deferReply();
+
+        await interaction.guild.roles.fetch().then(async roles => {
+          const role = roles.find(role => role.name === 'Obelisk Permission');
+
+          if (!role || !interaction.member.roles.cache.has(role.id)) {
+            const embed = new EmbedBuilder()
+              .setColor('#e67e22')
+              .setDescription(`**Unauthorized Access**\nYou do not have the required permissions.\nPlease ask an administrator for access.\n${role}\n\n ** Additional Information **\nThe role was generated upon token setup.`)
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+
+            return interaction.followUp({ embeds: [embed], ephemeral: true });
+          };
+
+          let filtered = 0;
+          const validService = async (services) => {
+            const button = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setLabel('Restart Cluster')
+                  .setCustomId('restart-cluster')
+                  .setStyle(ButtonStyle.Success)
+                  .setDisabled(false),
+
+                new ButtonBuilder()
+                  .setLabel('Stop Cluster')
+                  .setCustomId('stop-cluster')
+                  .setStyle(ButtonStyle.Secondary)
+                  .setDisabled(false),
+              );
+
+            services.forEach(service => {
+              if (platforms[service.details.folder_short] && service.status !== 'suspended') { filtered++ };
+            });
+
+            const embed = new EmbedBuilder()
+              .setColor('#2ecc71')
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+              .setDescription(`**Pending Action Authorization**\nGrant permission to access your services.\nPerform a cluster-wide server action.\n\`🟠\` \`${filtered} Gameservers Pending\`\n\n**Additional Information**\nDelete this message to return.`)
+
+            await interaction.followUp({ embeds: [embed], components: [button], ephemeral: false });
+          }
+
+          const validToken = async (nitrado) => {
+            const url = 'https://api.nitrado.net/services';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            const services = response.data.data.services;
+            response.status === 200 ? validService(services) : invalidService()
+          }
+
+          const validDocument = async ({ nitrado }) => {
+            const url = 'https://oauth.nitrado.net/token';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            response.status === 200 ? validToken(nitrado) : console.log('Invalid token'), null;
+          }
+
+          const reference = (await db.collection('ase-configuration').doc(interaction.guild.id).get()).data()
+          reference ? validDocument(reference) : console.log('Error')
+        });
+      };
+
+      if (interaction.customId === 'restart-cluster') {
+        await interaction.deferReply();
+
+        await interaction.guild.roles.fetch().then(async roles => {
+          const role = roles.find(role => role.name === 'Obelisk Permission');
+
+          if (!role || !interaction.member.roles.cache.has(role.id)) {
+            const embed = new EmbedBuilder()
+              .setColor('#e67e22')
+              .setDescription(`**Unauthorized Access**\nYou do not have the required permissions.\nPlease ask an administrator for access.\n${role}\n\n ** Additional Information **\nThe role was generated upon token setup.`)
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+
+            return interaction.followUp({ embeds: [embed], ephemeral: true });
+          };
+
+          const message = await interaction.message;
+
+          const success = async (data) => {
+
+            const button = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setLabel('Restart Cluster')
+                  .setCustomId('restart-cluster')
+                  .setStyle(ButtonStyle.Success)
+                  .setDisabled(true),
+
+                new ButtonBuilder()
+                  .setLabel('Stop Cluster')
+                  .setCustomId('stop-cluster')
+                  .setStyle(ButtonStyle.Secondary)
+                  .setDisabled(true),
+              );
+
+            const embed = new EmbedBuilder()
+              .setColor('#2ecc71')
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+              .setDescription(`**Pending Action Authorization**\nGrant permission to access your services.\nPerform a cluster-wide server action.\n\`🟢\` \`${data.length} Gameservers Restarting\`\n\n**Additional Information**\nDeletion:  <t:${Math.floor(Date.now() / 1000) + 15}:R>`)
+
+            const info = await interaction.followUp({ content: 'Data Fetch Success - API Online', ephemeral: false })
+            await message.edit({ embeds: [embed], components: [button] })
+              .then(() => setTimeout(() => { message.delete(); info.delete(); }, 15000));
+          };
+
+          const validService = async (nitrado, services) => {
+            const promise = await services.map(async service => {
+              if (platforms[service.details.folder_short] && service.status !== 'suspended') {
+                const url = `https://api.nitrado.net/services/${service.id}/gameservers/restart`;
+                return axios.post(url, { message: 'Obelisk Cluster-Wide Restart' }, { headers: { 'Authorization': nitrado.token } });
+              };
+            });
+
+            const data = [];
+            const responses = await Promise.all(promise);
+            responses.forEach(responses => { if (responses) { data.push(responses.status) } });
+            success(data)
+          };
+
+          const validToken = async (nitrado) => {
+            const url = 'https://api.nitrado.net/services';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            const services = response.data.data.services;
+            response.status === 200 ? validService(nitrado, services) : invalidService()
+          };
+
+          const validDocument = async ({ nitrado }) => {
+            const url = 'https://oauth.nitrado.net/token';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            response.status === 200 ? validToken(nitrado) : console.log('Invalid token'), null;
+          };
+
+          const reference = (await db.collection('ase-configuration').doc(interaction.guild.id).get()).data()
+          reference ? validDocument(reference) : console.log('Error')
+        });
+      };
+
+      if (interaction.customId === 'stop-cluster') {
+        await interaction.deferReply();
+
+        await interaction.guild.roles.fetch().then(async roles => {
+          const role = roles.find(role => role.name === 'Obelisk Permission');
+
+          if (!role || !interaction.member.roles.cache.has(role.id)) {
+            const embed = new EmbedBuilder()
+              .setColor('#e67e22')
+              .setDescription(`**Unauthorized Access**\nYou do not have the required permissions.\nPlease ask an administrator for access.\n${role}\n\n ** Additional Information **\nThe role was generated upon token setup.`)
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+
+            return interaction.followUp({ embeds: [embed], ephemeral: true });
+          };
+
+          const message = await interaction.message;
+
+          const success = async (data) => {
+
+            const button = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setLabel('Restart Cluster')
+                  .setCustomId('restart-cluster')
+                  .setStyle(ButtonStyle.Success)
+                  .setDisabled(true),
+
+                new ButtonBuilder()
+                  .setLabel('Stop Cluster')
+                  .setCustomId('stop-cluster')
+                  .setStyle(ButtonStyle.Secondary)
+                  .setDisabled(true),
+              );
+
+            const embed = new EmbedBuilder()
+              .setColor('#2ecc71')
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+              .setDescription(`**Pending Action Authorization**\nGrant permission to access your services.\nPerform a cluster-wide server action.\n\`🟢\` \`${data.length} Gameservers Stopping\`\n\n**Additional Information**\nDeletion:  <t:${Math.floor(Date.now() / 1000) + 15}:R>`)
+
+            const info = await interaction.followUp({ content: 'Data Fetch Success - API Online', ephemeral: false })
+            await message.edit({ embeds: [embed], components: [button] })
+              .then(() => setTimeout(() => { message.delete(); info.delete(); }, 13000));
+          };
+
+          const validService = async (nitrado, services) => {
+            const promise = await services.map(async service => {
+              if (platforms[service.details.folder_short] && service.status !== 'suspended') {
+                const url = `https://api.nitrado.net/services/${service.id}/gameservers/stop`;
+                return axios.post(url, { message: 'Obelisk Cluster-Wide Stop' }, { headers: { 'Authorization': nitrado.token } });
+              };
+            });
+
+            const data = [];
+            const responses = await Promise.all(promise);
+            responses.forEach(responses => { if (responses) { data.push(responses.status) } });
+            success(data)
+          };
+
+          const validToken = async (nitrado) => {
+            const url = 'https://api.nitrado.net/services';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            const services = response.data.data.services;
+            response.status === 200 ? validService(nitrado, services) : invalidService()
+          };
+
+          const validDocument = async ({ nitrado }) => {
+            const url = 'https://oauth.nitrado.net/token';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
+            response.status === 200 ? validToken(nitrado) : console.log('Invalid token'), null;
+          };
+
+          const reference = (await db.collection('ase-configuration').doc(interaction.guild.id).get()).data()
+          reference ? validDocument(reference) : console.log('Error')
+        });
+      };
+
+      if (interaction.customId === 'auto-maintanance') {
+        const platforms = { arkxb: true, arkps: true, arkse: true, arksa: true, arkswitch: true };
+
+        const gameserver = async (reference, services) => {
+
+          const filter = async (service) => {
+            platforms[service.details.folder_short] && service.status === 'stopped' ? console.log('Status: Stopped') : console.log('Status: Online')
+          };
+
+          const tasks = await services.map(async service => {
+            const url = `https://api.nitrado.net/services/${service.id}/gameservers`;
+            const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } })
+            if (response.status === 200) { await filter(service, response.data.data.gameserver) };
+          });
+        }
+
+        const service = async (reference) => {
+          try {
+            const url = 'https://api.nitrado.net/services';
+            const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+            response.status === 200 ? gameserver(reference, response.data.data.services) : unauthorized();
+          } catch (error) { unauthorized() };
+        };
+
+        const token = async (reference) => {
+          try {
+            const url = 'https://oauth.nitrado.net/token';
+            const response = await axios.get(url, { headers: { 'Authorization': reference.nitrado.token } });
+            response.status === 200 ? service(reference) : unauthorized();
+          } catch (error) { unauthorized() };
+        };
+
+        const reference = (await db.collection('ase-configuration').doc(interaction.guild.id).get()).data();
+        reference ? await token(reference) : unauthorized();
+      };
+    });
+  },
+};
---
+++ source/event-handler/listener-setup.js
@@ -0,0 +1,243 @@
+const { ActionRowBuilder, Events, ModalBuilder, ChannelType, TextInputBuilder, TextInputStyle, EmbedBuilder, ButtonBuilder, ButtonStyle, PermissionFlagsBits } = require('discord.js');
+const { db } = require('../script.js');
+const axios = require('axios');
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    client.on(Events.InteractionCreate, async interaction => {
+
+      try {
+        if (interaction.customId === 'setup-token') {
+          const modal = new ModalBuilder()
+            .setCustomId('token-modal')
+            .setTitle('Nitrado Token Verification');
+
+          const row = new ActionRowBuilder()
+            .addComponents(
+              new TextInputBuilder()
+                .setCustomId('token-option').setLabel('Required Nitrado Token').setMinLength(50).setMaxLength(150)
+                .setPlaceholder('...oAg66TcQYUnYXBQn17A161-N86cN5jWDp7')
+                .setStyle(TextInputStyle.Short)
+                .setRequired(true)
+            );
+
+          modal.addComponents(row);
+          await interaction.showModal(modal);
+        }
+
+        if (interaction.customId === 'token-modal') {
+          const invalidToken = async () => {
+            await interaction.reply({ content: 'Setup failure, ensure you follow provided steps above.', ephemeral: true });
+          }
+
+          const validToken = async ({ token }) => {
+            const button = new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                  .setLabel('Base Version')
+                  .setCustomId('base-version')
+                  .setStyle(ButtonStyle.Success),
+
+                new ButtonBuilder()
+                  .setLabel('Upgraded Version')
+                  .setCustomId('upgraded-version')
+                  .setStyle(ButtonStyle.Secondary)
+                  .setDisabled(true),
+              );
+
+            const embed = new EmbedBuilder()
+              .setColor('#2ecc71')
+              .setDescription(`**Token Creation & Overview**\n For those interested in upgrading to our more advanced tooling, the payment __must__ be done before installation, the base version is free.\n\n**Additional Information**\nSelect the upgrade button below for those who want our premium version. After the payment, press it again for a seamless transition.\n\n__Premium temporarily disabled.__ \n\n**[Partnership & Information](https://nitra.do/obeliskdevelopment "Nitrado Partner Link")**\nConsider using our partnership link to purchase your personal servers to help fund our services!`)
+              .setFooter({ text: 'Tip: Contact support if there are issues.' })
+              .setImage('https://i.imgur.com/2ZIHUgx.png')
+
+            await interaction.reply({ embeds: [embed], components: [button], ephemeral: true }).then(async () => {
+              await db.collection('ase-configuration').doc(interaction.guild.id)
+                .set({ ['nitrado']: { token: token } }, { merge: true })
+            });
+          };
+
+          try {
+            const nitrado = { token: interaction.fields.getTextInputValue('token-option') };
+
+            const url = 'https://oauth.nitrado.net/token';
+            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
+            response.status === 200 && interaction.guild.features.includes('COMMUNITY')
+              ? validToken(nitrado) : invalidToken();
+
+          } catch (error) { invalidToken(), null; }
+        };
+
+        if (interaction.customId === 'base-version') {
+          const installation = await interaction.reply({ content: 'Installing...', ephemeral: true });
+
+          const roles = await interaction.guild.roles.fetch();
+          const action = roles.map(async role => role.name === 'Obelisk Permission' ? await role.delete() : null);
+          try { await Promise.all(action) } catch (error) { return await installation.edit({ content: 'In your settings, move the bot role above the newly generated permission role.', ephemeral: true }) };
+
+          await interaction.guild.roles.create({
+            name: 'Obelisk Permission',
+            color: '#ffffff',
+          }).then(() => console.log('Role created...'));
+
+          const permissions = [{
+            id: interaction.guild.id,
+            deny: [PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ReadMessageHistory],
+          }];
+
+          const statistics = await interaction.guild.channels.create({
+            name: `AS:E Cluster Statistics`,
+            type: ChannelType.GuildCategory,
+            permissionOverwrites: permissions
+          });
+
+          const players = await interaction.guild.channels.create({
+            name: 'Active: 0 Players',
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: permissions,
+            parent: statistics
+          });
+
+          const active = await interaction.guild.channels.create({
+            name: 'Active: 0 Servers',
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: permissions,
+            parent: statistics
+          });
+
+          const outage = await interaction.guild.channels.create({
+            name: 'Outage: 0 Servers',
+            type: ChannelType.GuildVoice,
+            permissionOverwrites: permissions,
+            parent: statistics
+          });
+
+          const management = await interaction.guild.channels.create({
+            name: `AS:E Obelisk Management`,
+            type: ChannelType.GuildCategory,
+            permissionOverwrites: permissions
+          });
+
+          await interaction.guild.channels.create({
+            name: '⚫│𝗕ot-𝗖ommands',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: management
+          });
+
+          const status = await interaction.guild.channels.create({
+            name: '⚫│𝗦erver-𝗦tatus',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: management
+          });
+
+          let embed = new EmbedBuilder()
+            .setColor('#2ecc71')
+            .setDescription(`**Cluster Status Installation**\nThe status page is currently being installed. \nBelow is the expected processing timer. \n\nEstimated: <t:${Math.floor(Date.now() / 1000) + 300}:R>`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setImage('https://i.imgur.com/2ZIHUgx.png')
+
+          const message = await status.send({ embeds: [embed] });
+
+          const audits = await interaction.guild.channels.create({
+            name: `AS:E Audit Logging`,
+            type: ChannelType.GuildCategory,
+            permissionOverwrites: permissions
+          });
+
+          const player = await interaction.guild.channels.create({
+            name: '📄│𝗣layer-𝗖ommands',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: audits
+          });
+
+          const server = await interaction.guild.channels.create({
+            name: '📄│𝗦erver-𝗖ommands',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: audits
+          });
+
+          const logging = await interaction.guild.channels.create({
+            name: `AS:E Game Logging `,
+            type: ChannelType.GuildCategory,
+            permissionOverwrites: permissions
+          });
+
+          const admin = await interaction.guild.channels.create({
+            name: '📑│𝗔dmin-𝗟ogging',
+            type: ChannelType.GuildForum,
+            permissionOverwrites: permissions,
+            parent: logging
+          });
+
+          const chat = await interaction.guild.channels.create({
+            name: '📑│𝗖hat-𝗟ogging',
+            type: ChannelType.GuildForum,
+            permissionOverwrites: permissions,
+            parent: logging
+          });
+
+          const button = new ActionRowBuilder()
+            .addComponents(
+              new ButtonBuilder()
+                .setLabel('Admin Logging')
+                .setCustomId('admin-thread-modal')
+                .setStyle(ButtonStyle.Success)
+                .setDisabled(false),
+
+              new ButtonBuilder()
+                .setLabel('Chat Logging')
+                .setCustomId('chat-thread-modal')
+                .setStyle(ButtonStyle.Secondary)
+                .setDisabled(false),
+            );
+
+          embed = new EmbedBuilder()
+            .setColor('#2ecc71')
+            .setDescription(`**Logging Creation Tooling**\nSelect the buttons below for those interested in utilizing our logging services and begin entering your service identifiers for each gameserver.\n\n**Additional Information**\nActive servers generate faster-flowing logs. The bot checks for new entries every 5 minutes, while some gameservers may take upwards of one hour.`)
+            .setFooter({ text: 'Tip: Contact support if there are issues.' })
+            .setImage('https://i.imgur.com/2ZIHUgx.png')
+
+          const process = await interaction.guild.channels.create({
+            name: '📑│𝗜nstallation',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: logging
+          });
+
+          await process.send({ embeds: [embed], components: [button] });
+
+          const protections = await interaction.guild.channels.create({
+            name: `AS:E Protections `,
+            type: ChannelType.GuildCategory,
+            permissionOverwrites: permissions
+          });
+
+          const dupe = await interaction.guild.channels.create({
+            name: '🔗│𝗗upe-𝗗etection',
+            type: ChannelType.GuildText,
+            permissionOverwrites: permissions,
+            parent: protections
+          });
+
+          await db.collection('ase-configuration').doc(interaction.guild.id)
+            .set({
+              ['statistics']: { players: players.id, active: active.id, outage: outage.id },
+              ['status']: { channel: status.id, message: message.id },
+              ['audits']: { server: server.id, player: player.id },
+              ['forum']: { chat: chat.id, admin: admin.id },
+
+              ['protections']: { channel: dupe.id }
+            }, { merge: true });
+
+          await installation.edit({ content: 'Installation complete...', ephemeral: true });
+        };
+      } catch (error) { console.log(error) };
+    });
+  },
+};

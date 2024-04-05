6fa399e3d047ef040ffac992829a3e94504ae916
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-04-05 17:40:08 -0600

    Changed regex pattern.

--- source/event-handler/event-logging/logging-logic/module-join.js
+++ source/event-handler/event-logging/logging-logic/module-join.js
@@ -2,21 +2,35 @@ const { EmbedBuilder } = require('discord.js');
 const data = new Set();
 
 const joinExtractionLogic = async (reference, service, response, client) => {
-  const regex = /\[\d{4}\.\d{2}\.\d{2}-\d{2}\.\d{2}\.\d{2}:\d{3}\]\[\s*\d+\]([\d.]+_[\d.]+): (\S+(?: \S+)*) \(([^()]+)\): (.+)/g;
-  console.log('Join')
+  const regex = /.\d{4}.\d{2}.\d{2}.\d{2}.\d{2}.\d{2}.\d{3}..\d{3}.(\d{4}.\d{2}.\d{2}.\d{2}.\d{2}.\d{2}).\s+(\w{0,})\s*(\w{0,})\s*\w{0,}\s*ARK/g;
+
   try {
     let counter = 0;
     let result = '', pattern = '', unique = '';
     while ((result = regex.exec(response)) !== null && counter <= 10) {
-      const [string, date, gamertag, username, message] = result;
+      const [string, date, gamertag, condition] = result;
       const [datePart, timePart] = date.split('_');
       const dateTimeString = `${datePart.replace(/\./g, '-')}T${timePart.replace(/\./g, ':')}`;
       const unix = Math.floor(new Date(dateTimeString).getTime() / 1000);
 
-      pattern += `<t:${unix}:f>\n**Player Identity Information**\n[${gamertag}]: ${username} \n${message} \n\n`;
-      if (!data.has(pattern)) {
-        data.add(pattern), counter++;
-        unique += `<t:${unix}:f>\n**Player Identity Information**\n[${gamertag}]: ${username} \n${message} \n\n`;
+      switch (condition) {
+        case 'joined':
+          pattern += `<t:${unix}:F>\n\`\`\`\n🟢 ${gamertag} joined your server!\n\`\`\`\n`;
+          if (!data.has(pattern)) {
+            data.add(pattern), counter++;
+            unique += `<t:${unix}:F>\n\`\`\`\n🟢 ${gamertag} joined your server!\n\`\`\`\n`;
+          };
+          break;
+        case 'left':
+          pattern += `<t:${unix}:F>\n\`\`\`\n🟠 ${gamertag} left your server!\n\`\`\`\n`;
+          if (!data.has(pattern)) {
+            data.add(pattern), counter++;
+            unique += `<t:${unix}:F>\n\`\`\`\n🟠 ${gamertag} left your server!\n\`\`\`\n`;
+          };
+          break;
+
+        default:
+          break;
       };
     };
 
@@ -31,9 +45,7 @@ const joinExtractionLogic = async (reference, service, response, client) => {
             .setDescription(`${unique}`);
 
           await channel.send({ embeds: [embed] });
-        } catch (error) {
-          if (error.code === 10003) { null };
-        };
+        } catch (error) { { null } };
       };
     });
   } catch (error) { null };

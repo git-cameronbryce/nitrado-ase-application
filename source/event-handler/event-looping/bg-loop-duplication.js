e8356ad0d6058fdb91969432f4ea65b750f385c0
git-cameronbryce <developmentcameronbryce@gmail.com>
2024-04-28 15:05:41 -0600

    Changed dupe detection feature.

--- source/event-handler/event-looping/bg-loop-duplication.js
+++ source/event-handler/event-looping/bg-loop-duplication.js
@@ -1,55 +1,62 @@
-const { ActionRowBuilder, Events, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { Events } = require('discord.js');
+const rateLimit = require('axios-rate-limit');
 const { db } = require('../../script');
 const axios = require('axios');
 
-const platforms = { arkxb: true };
-const matches = new Set();
+process.on('unhandledRejection', (error) => console.error(error));
+
+const platforms = { arkxb: true, arkps: true, arkse: true, arkswitch: true };
+const api = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 250 });
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
-
     async function loop() {
-      const platforms = { arkxb: true, arkps: true };
 
-      const gameserver = async (nitrado, protections, services) => {
-        const verify = async (player) => {
-          matches.add(player.name);
-          if (!matches.has(player.name)) {
-            console.log(player.name);
-          }
-        };
+      const gameserver = async (nitrado, statistics, services) => {
+        let players = 0, active = 0, outage = 0;
+        const parse = async (gameserver) => {
+          const { query, status } = gameserver;
+          if (status === 'suspended') { return };
 
-        const filter = async (service, gameserver) => {
-          const url = `https://api.nitrado.net/services/${service.id}/gameservers/games/players`;
-          const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
-          response.data.data.players.forEach(async player => {
-            if (player.online) { await verify(player) }
-          });
+          if (query.player_current) { players += query.player_current };
+          if (status === 'started') { active++ }
+          if (status === 'stopped') { outage++ }
         };
 
         const tasks = await services.map(async service => {
-          if (platforms[service.details.folder_short]) {
-            const url = `https://api.nitrado.net/services/${service.id}/gameservers`;
-            const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } });
-            if (response.status === 200 && response.data.data.gameserver.status === 'started') { await filter(service, response.data.data.gameserver) };
+          const url = `https://api.nitrado.net/services/${service.id}/gameservers`;
+          const response = await api.get(url, { headers: { 'Authorization': nitrado.token } });
+          if (response.status === 200 && platforms[response.data.data.gameserver.game]) {
+            await parse(response.data.data.gameserver);
           };
         });
+
+        await Promise.all(tasks).then(async () => {
+          try {
+            const playerVoice = await client.channels.fetch(statistics.players);
+            const activeVoice = await client.channels.fetch(statistics.active);
+            const outageVoice = await client.channels.fetch(statistics.outage);
+
+            await playerVoice.setName(`Active: ${players} Players`);
+            await activeVoice.setName(`Active: ${active} Servers`);
+            await outageVoice.setName(`Outage: ${outage} Servers`);
+          } catch (error) { null };
+        })
       };
 
-      const service = async (nitrado, protections) => {
+      const service = async (nitrado, statistics) => {
         const url = 'https://api.nitrado.net/services';
-        const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
-        const services = response.data.data.services;
-        response.status === 200 ? gameserver(nitrado, protections, services) : invalidService()
+        const response = await api.get(url, { headers: { 'Authorization': nitrado.token } })
+        if (response.status === 200) gameserver(nitrado, statistics, response.data.data.services);
       };
 
-      const token = async ({ nitrado, protections }) => {
+      const token = async ({ nitrado, statistics }) => {
         try {
           const url = 'https://oauth.nitrado.net/token';
-          const response = await axios.get(url, { headers: { 'Authorization': nitrado.token } })
-          response.status === 200 ? service(nitrado, protections) : console.log('Invalid token');
+          const response = await api.get(url, { headers: { 'Authorization': nitrado.token } })
+          if (response.status === 200) service(nitrado, statistics);
         } catch (error) { null };
       };
 
@@ -57,8 +64,8 @@ module.exports = {
       reference.forEach(doc => {
         doc.data() ? token(doc.data()) : console.log('Invalid document.');
       });
-      setTimeout(loop, 120000);
+      setTimeout(loop, 15000)
     };
-    // loop().then(() => console.log('Loop started:'));
+    loop().then(() => console.log('Loop started:'));
   },
 };
